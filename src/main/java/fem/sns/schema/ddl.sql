b3e0d0f2b6ca0a41b9f59b21d642153646443fe8
lee <lsg0814k@gmail.com>
2024-04-10 23:07:59 +0900

    Follow 테이블 추가

--- src/main/java/fem/sns/schema/ddl.sql
+++ src/main/java/fem/sns/schema/ddl.sql
@@ -17,3 +17,12 @@ create table MemberNicknameHistory
   constraint memberNicknameHistory_id_uindx
         primary key (id)
 );
+
+create table Follow (
+    id int auto_increment,
+    fromMemberId int not null,
+    toMemberId int not null,
+    createAt datetime not null,
+    constraint follow_id_uindx
+        primary key (id)
+);
