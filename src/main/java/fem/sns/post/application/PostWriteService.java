9716c84e70383e7f620c629a555ba190bd496214
lee <lsg0814k@gmail.com>
2024-04-11 14:26:56 +0900

    transactional 추가

--- src/main/java/fem/sns/post/application/PostWriteService.java
+++ src/main/java/fem/sns/post/application/PostWriteService.java
@@ -5,8 +5,10 @@ import fem.sns.post.domain.Post;
 import fem.sns.post.domain.PostCreate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class PostWriteService {
     private final PostRepository postRepository;
